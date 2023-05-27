228370a9024518aafbe5ee6bcaa13b685ffb14d7
AdrianCapolicchio <128909031+AdrianCapolicchio@users.noreply.github.com>
2023-05-27 20:27:04 -0300

    Add files via upload
    agregue js

---
+++ js/comprarTiquets.js
@@ -0,0 +1,29 @@
+function calcular() {
+
+let nro1= document.getElementById( "Numero1").value;
+console.log ("Numero1= "+ nro1 );
+let operando= document.getElementById("selectOper").value;
+let total=0;
+
+if (operando == "General"){
+    total = parseInt(nro1)*200;
+}
+if (operando == "Estudiante"){
+    total = parseInt(nro1)*0.20*200;
+}
+if (operando == "Trainee"){
+    total = parseInt(nro1)*0.50*200;
+}
+if (operando == "Junior"){
+    total = parseInt(nro1)*0.85*200;
+}
+
+
+console.log (total);
+
+document.getElementById("resultado").innerHTML = "Total a Pagar: $ " + total;
+
+
+
+
+}
