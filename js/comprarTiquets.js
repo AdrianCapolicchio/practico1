ad5a1dfe54a654ca36d2a9e114dbf627b8fcf968
AdrianCapolicchio <128909031+AdrianCapolicchio@users.noreply.github.com>
2023-05-28 11:26:05 -0300

    Update comprarTiquets.js
    js recarga.

--- js/comprarTiquets.js
+++ js/comprarTiquets.js
@@ -7,16 +7,16 @@ let total=0;
 
 if (operando == "General"){
     total = parseInt(nro1)*200;
-}
+    }
 if (operando == "Estudiante"){
     total = parseInt(nro1)*0.20*200;
-}
+    }
 if (operando == "Trainee"){
     total = parseInt(nro1)*0.50*200;
-}
+    }
 if (operando == "Junior"){
     total = parseInt(nro1)*0.85*200;
-}
+    }
 
 
 console.log (total);
